608a3d41ea406a9b97b36369795b03b6e5ed36e2
Austin McCarthy <austindmccarthy@gmail.com>
2020-01-07 17:11:05 -0700

    Cleaned data

--- src/clean_data.py
+++ src/clean_data.py
@@ -2,10 +2,16 @@ import pandas as pd
 import os
 
 def cleaner():
-    for file in os.listdir('../Text_Data'):
-        df = pd.read_csv(os.path.join('../Text_Data/',file), header=3, sep='|')
+    for file in os.listdir('../Usage_Data'):
+        df = pd.read_csv(os.path.join('../Usage_Data/',file), header=3, sep='|')
         df = df.drop(index=[0, len(df)-1])
-        df.to_csv(file.strip('.txt')+'.csv')
+        df = df[df.columns[[1,2,3]]]
+        df = df.rename(columns={df.columns[0]:'Rank', df.columns[1]:'Pokemon', df.columns[2]:'Weighted Usage %'})
+        df['Pokemon'] = df['Pokemon'].apply(lambda x: x.strip())
+        df['Rank'] = df['Rank'].astype(int)
+        df['Weighted Usage %'] = df['Weighted Usage %'].apply(lambda x: x.replace('%', '')).astype(float)
+        df = df[df['Weighted Usage %'] >= 1]
+        df.to_csv(file.strip('.txt')+'.csv', index=False)
 
 if __name__ == '__main__':
     cleaner()
--- src/get_data.py
+++ src/get_data.py
@@ -1,7 +1,7 @@
 import requests
 
 def make_url(month):
-    if month == ('2016-11' or '2016-12' or '2017-01'):
+    if month == '2016-11' or month == '2016-12' or month == '2017-01':
         singles_url = 'https://www.smogon.com/stats/' + month + '/gen7pokebankou-1825.txt'
         doubles_url = 'https://www.smogon.com/stats/' + month + '/gen7pokebankdoublesou-1825.txt'
         return (singles_url, doubles_url)
@@ -29,7 +29,7 @@ month_list = ['2016-11', '2016-12', '2017-01', '2017-02', '2017-03', '2017-04',\
               '2017-11', '2017-12', '2018-01', '2018-02', '2018-03', '2018-04',\
               '2018-05', '2018-06', '2018-07', '2018-08', '2018-09', '2018-10',\
               '2018-11', '2018-12', '2019-01', '2019-02', '2019-03', '2019-04',\
-              '2019-05', '2019-06', '2019-07', '2019-08', '2019-09', '2019-10',]
+              '2019-05', '2019-06', '2019-07', '2019-08', '2019-09', '2019-10']
 
 if __name__ == '__main__':
     save_data(month_list)
